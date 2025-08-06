3cf0f3d2e67ce064c03c312a2eedc3b479484c31
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2025-08-05 19:24:25 -0500

    clase #1

---
+++ 2-Python/Ejercicios/2-Tipo de Datos en Python/2.1Tipodedatos.py
@@ -0,0 +1,44 @@
+"""
+Tipo de datos en Python
+#------------1.Numericos----------------
+entero =10  int
+decimal = 3.14  float
+complejo = 2+3j  complex
+#-----------2.Cadena--------------------
+texto = "Hola Python"  str
+respuesta = 'Es Un Gusto tenerte por aca'
+#-----------3.Booleano-------------------
+verdadero= True  bool
+falso= False
+#-----------4.Listas (Ordenas,modificables)--------
+lista = [1,2,4,"python",True]
+#-----------5.Tuplas(ordenadas,inmutables)---------
+tupla = (10,20,30)
+#-----------6.Conjuntos(no ordenados,sin duplicados)---------
+conjunto = {1,2,3,4}
+#-----------7.Diccionarios(clave:valor)-----------------------
+diccionario = {"nombre":"Armando","edad":36,"Lenguaje":"python"}
+
+"""
+base = 10
+#print(type(base))
+altura = 5.5
+area = base * altura
+#print(f"El Area del Triangulo es {area}")
+
+
+#nombre = input("Escribe tu nombre:")
+#print("mayusculas:",nombre.upper())
+#print("minusculas:",nombre.lower())
+#print("cantidad de caracteres:",len(nombre))
+
+
+#edad = int(input("Cual es tu edad:"))
+#print(type(edad))
+#es_mayor=edad>=18
+#print(f"Es Mayor de edad {es_mayor}")
+
+numero = 10
+texto= "5"
+
+resultado = numero + int(texto)
